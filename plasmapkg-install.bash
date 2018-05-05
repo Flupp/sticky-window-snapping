b860d4a51e251ec4f761cee1f799e308bebbaa0c
Toni Dietze <sticky-window-snapping@derflupp.e4ward.com>
2018-05-05 18:14:31 +0200

    plasmapkg-{,un}install.bash: manually install *.desktop to kservices5/
    If the desktop file is not put in kservices5/, then the config dialog is not available in the kcmshell.

--- plasmapkg-install.bash
+++ plasmapkg-install.bash
@@ -2,4 +2,5 @@
 
 set -x
 
-plasmapkg2 --type kwinscript --install .
+plasmapkg2 --type KWin/Script --install .
+cp metadata.desktop ~/.local/share/kservices5/kwin-script-sticky-window-snapping.desktop
--- plasmapkg-uninstall.bash
+++ plasmapkg-uninstall.bash
@@ -2,8 +2,8 @@
 
 set -x
 
-plasmapkg2 --type kwinscript --remove .
-plasmapkg  --type kwinscript --remove .
+plasmapkg2 --type KWin/Script --remove .
+plasmapkg  --type KWin/Script --remove .
 rm --recursive --verbose  \
 	~/.local/share/kwin/scripts/sticky-window-snapping  \
 	~/.local/share/kservices5/kwin-script-sticky-window-snapping.desktop
