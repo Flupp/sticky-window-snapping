3fc153fe00650c7b34368e21b3fb0a59b3ef80b3
Toni Dietze <sticky-window-snapping@derflupp.e4ward.com>
2018-05-27 17:52:20 +0200

    plasmapkg-install.bash: use symbolic link instead of file copy

--- plasmapkg-install.bash
+++ plasmapkg-install.bash
@@ -3,4 +3,4 @@
 set -x
 
 plasmapkg2 --type KWin/Script --install .
-cp metadata.desktop ~/.local/share/kservices5/kwin-script-sticky-window-snapping.desktop
+ln --relative --symbolic ~/.local/share/kwin/scripts/sticky-window-snapping/metadata.desktop ~/.local/share/kservices5/kwin-script-sticky-window-snapping.desktop
